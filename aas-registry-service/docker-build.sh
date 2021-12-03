cea13ce6ba233cf02ca523ce9ba4bae9d34ce0fc
Daniel Porta <daniel.porta@dfki.de>
2021-12-03 08:51:50 +0100

    renamed docker image

--- aas-registry-service/docker-build.sh
+++ aas-registry-service/docker-build.sh
@@ -3,5 +3,5 @@
 cp target/aas-registry-service-0.0.1-SNAPSHOT.jar docker/aas-registry-service.jar
 cd docker
 
-docker build -t aas-registry/latest .
+docker build -t dfkibasys/aas-registry:dotaas .
 echo "Done"
