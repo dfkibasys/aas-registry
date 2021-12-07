4adba54bdcba8379965f63a15cf798d59bb8bcbd
Daniel Porta <daniel.porta@dfki.de>
2021-12-07 15:18:15 +0100

    refined docker provisioning

--- aas-registry-service/docker-build.sh
+++ aas-registry-service/docker-build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-cp target/aas-registry-service-0.0.1-SNAPSHOT.jar docker/aas-registry-service.jar
-cd docker
-
 docker build -t dfkibasys/aas-registry:dotaas .
-echo "Done"
+read -p "Done. Press any key to continue... " -n1 -s
--- aas-registry-service/docker-compose-up.sh
+++ aas-registry-service/docker-compose-up.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd docker
 docker-compose up -d --build --force-recreate
